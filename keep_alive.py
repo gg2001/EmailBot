57b2f9e0f442b280a5e57d8f9c13692f201c4104
Gautham Ganesh Elango <gautham.gg@gmail.com>
2020-08-19 17:00:15 +1000

    sqlite dumps

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,26 @@
 from flask import Flask
 from threading import Thread
+import sqlite3
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask('')
 
 @app.route('/')
 def home():
     return "EmailBot is running"
+
+def backup_db():
+    conn = sqlite3.connect('bot.db')
+    c = conn.cursor()
+    with open('dump.sql', 'w') as f:
+        for line in conn.iterdump():
+            f.write('%s\n' % line)
+    conn.close()
+    print("Sqlite3 Dumped")
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(backup_db,'interval',minutes=1)
+sched.start()
 
 def run():
   app.run(host='0.0.0.0',port=8080)
